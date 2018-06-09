8eb350ad76223150cec8d8e4036d5b096f148894
Romain MURA <romain@mura.io>
2018-06-09 23:27:00 +0200

    refactored

--- LI-WaterTanks.ino
+++ LI-WaterTanks.ino
@@ -1,20 +1,25 @@
-#include <Adafruit_NeoPixel.h>
+
 
 #include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
 #include <Adafruit_GFX.h>
-#include "Adafruit_LEDBackpack.h"
+#include <Adafruit_LEDBackpack.h>
+#include <Adafruit_NeoPixel.h>
 #include <HCSR04.h>
 
 #define left_stop 10
 #define right_stop 9
+#define left_pwr 7
+#define right_pwr 8
+#define left_active 6
+#define right_active 5
 
 const int x = 16; //distance between sensor and higher water level
+
 Adafruit_7segment matrix = Adafruit_7segment();
 UltraSonicDistanceSensor distanceSensor(11, 12);
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(22, 2, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
   strip.begin();
   strip.show();
   Serial.begin(9600);
@@ -34,33 +39,25 @@ void setup() {
 }
 
 void buzz_right(int duration){
-  digitalWrite(8, 1);
-  //digitalWrite(7, 1);
+  digitalWrite(right_pwr, 1);
   for(int i = 0; i <= duration; i++){
-    //digitalWrite(6, 1);
-    digitalWrite(5, 1);
+    digitalWrite(right_active, 1);
     delay(1);
-    //digitalWrite(6, 0);
-    digitalWrite(5, 0);
+    digitalWrite(right_active, 0);
     delay(1);
   }
-  digitalWrite(8, 0);
-  //digitalWrite(7, 0);
+  digitalWrite(right_pwr, 0);
 }
 
 void buzz_left(int duration){
-  //digitalWrite(8, 1);
-  digitalWrite(7, 1);
+  digitalWrite(left_pwr, 1);
   for(int i = 0; i <= duration; i++){
-    digitalWrite(6, 1);
-    //digitalWrite(5, 1);
+    digitalWrite(left_active, 1);
     delay(1);
-    digitalWrite(6, 0);
-    //digitalWrite(5, 0);
+    digitalWrite(left_active, 0);
     delay(1);
   }
-  //digitalWrite(8, 0);
-  digitalWrite(7, 0);
+  digitalWrite(left_pwr, 0);
 }
 
 //Min Level = 39cm
@@ -69,15 +66,14 @@ void loop() {
   if(digitalRead(left_stop) == 0){
     buzz_left(80);
   }
-  if(digitalRead(9) == 0){
+  if(digitalRead(right_stop) == 0){
     buzz_right(80);
   }
+  
   double dst = distanceSensor.measureDistanceCm();
-  //Serial.print(dst);
-  //Serial.print(" - ");
+  
   dst = round(dst);
   int dst2 = (int) dst;
-  //Serial.println(dst2);
   matrix.print(dst2 - 16);
   matrix.writeDisplay();
   int leds = dst2;
@@ -86,13 +82,14 @@ void loop() {
   leds = map(leds, x, 39, 3, 16);
   Serial.println(leds);
   for(int i =0; i < 23; i++){
-    strip.setPixelColor(i, 0,0,150);
+    strip.setPixelColor(i, 0, 0, 150);
   }
   for(int i =0; i < leds; i++){
-    strip.setPixelColor(i, 150,0,0);
+    strip.setPixelColor(i, 150, 0, 0);
   }
   strip.show();
   delay(50);
+  
   /*dst = round(dst);
   dst = dst - x;
   for(int i = 0; i <= 23; i++){
